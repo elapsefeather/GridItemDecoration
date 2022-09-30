16a4c97c81331ca3674c9d0904af2e3692aa98ee
羿婷 <e25824564@gmail.com>
2022-09-30 14:30:58 +0800

    [Add] exp

--- app/src/main/java/com/elapsefeather/griditemdecoration/MainActivity.java
+++ app/src/main/java/com/elapsefeather/griditemdecoration/MainActivity.java
@@ -1,14 +1,30 @@
 package com.elapsefeather.griditemdecoration;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    RecyclerView rv;
+    List<String> menu = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        for (int i = 1; i < 21; i++) {
+            menu.add("star" + i);
+        }
+        rv = findViewById(R.id.rv);
+        rv.setLayoutManager(new GridLayoutManager(this, 3, RecyclerView.VERTICAL, false));
+        rv.setAdapter(new Adapter(menu));
+        GridItemDecoration itemDec = new GridItemDecoration(this, GridItemDecoration.ROUNDALL);
+        itemDec.setDividerColor(getColor(R.color.black));
+        rv.addItemDecoration(itemDec);
     }
 }
